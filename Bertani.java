96c7eb27112fafc0ee8a5f466acc09cd51187d69
salwafz <xxsalwa.faizahxx@gmail.com>
2024-09-03 13:37:23 +0700

    Update Bertani.java

--- Bertani.java
+++ Bertani.java
@@ -1,13 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
- */
+
 package pertemuanKedua;
 
 /**
  *
  * @author Salwa Faizah
  */
-public interface bertani {
+public interface Bertani {
     public void memanen();
 }
