4255a02a8b98bf0aec22b6ee1d5623ee04baaea3
salwafz <xxsalwa.faizahxx@gmail.com>
2024-09-03 13:37:59 +0700

    Update Berternak.java

--- Berternak.java
+++ Berternak.java
@@ -1,13 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
- */
+
 package pertemuanKedua;
 
 /**
  *
  * @author Salwa Faizah
  */
-public interface berternak {
+public interface Brternak {
     public void ngarit();
 }
