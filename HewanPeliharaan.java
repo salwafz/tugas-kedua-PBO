5b2a4d7687a48a7be7d6c44ea9d6a6317246bca6
salwafz <xxsalwa.faizahxx@gmail.com>
2024-09-06 22:00:24 +0700

    Add files via upload

---
+++ HewanPeliharaan.java
@@ -0,0 +1,72 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pertemuanKedua;
+
+/**
+ *
+ * @author Salwa Faizah
+ */
+public class HewanPeliharaan extends Hewan implements Berkebun, Berternak, Bertani {
+
+    private String jenisMakanan;
+    private String caraBerproduksi;
+
+    public void IniHewan() {
+        System.out.println("Ini this hewan peliharaan");
+    }
+
+    public void HewanPeliharaan() {
+        System.out.println("ini kons Peliharaann");
+        this.setJenisMakanan("aku karnivora");
+        this.setCaraBerproduksi("aku hewan vivipar");
+        super.setJumlahKaki(4);
+        super.setCaraBernafas("menggunakan paru-paru");
+        super.IniHewan();
+        this.IniHewan();
+    }
+
+    /**
+     * @return the jenisMakanan
+     */
+    public String getJenisMakanan() {
+        return jenisMakanan;
+    }
+
+    /**
+     * @param jenisMakanan the jenisMakanan to set
+     */
+    public void setJenisMakanan(String jenisMakanan) {
+        this.jenisMakanan = jenisMakanan;
+    }
+
+    /**
+     * @return the caraBerproduksi
+     */
+    public String getCaraBerproduksi() {
+        return caraBerproduksi;
+    }
+
+    /**
+     * @param caraBerproduksi the caraBerproduksi to set
+     */
+    public void setCaraBerproduksi(String caraBerproduksi) {
+        this.caraBerproduksi = caraBerproduksi;
+    }
+
+    @Override
+    public void menanam() {
+        System.out.println("Aku makan apa saja");
+    }
+
+    @Override
+    public void ngarit() {
+        System.out.println("Aku ngarit untuk hewanku");
+    }
+
+    @Override
+    public void memanen() {
+        System.out.println("Aku memanen");
+    }
+}
---
+++ Utama.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pertemuanKedua;
+
+/**
+ *
+ * @author Salwa Faizah
+ */
+public class Utama {
+
+    public static void main(String[] args) {
+
+        HewanPeliharaan Anjing = new HewanPeliharaan();
+        Anjing.HewanPeliharaan();
+        System.out.println("INi dr anjing berkaki" +Anjing.getJumlahKaki());
+        System.out.println("");
+        
+        Kucing comel = new Kucing();
+        System.out.println("aku kucing " + comel.getRas() + ", bulu ku berwarna " + comel.getWarnaBulu()
+                +", kaki ku ada " +comel.getJumlahKaki());
+        System.out.println("");
+        
+        Hewan miaw = (Hewan) comel;
+        System.out.println("Aku kochengg berkaki " + miaw.getJumlahKaki() 
+                +", aku bernafas loohhh " +miaw.getCaraBernafas());
+        
+        System.out.println("");
+        
+        
+        //ini interface
+        
+        HewanPeliharaan dino = new HewanPeliharaan();
+        dino.menanam();
+        dino.ngarit();
+        dino.memanen();
+    }
+}
